9d51b3d63906ba78be7124dbfdc0bc57e0a769ef
rc3097 <rc3097@nyu.edu>
2015-12-03 19:46:34 -0500

    change model

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,108 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.AbstractCollection;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
-
+import java.util.NoSuchElementException;
 import nlp.util.CommandLineUtils;
 
 public class Main {
+
+	static class SentenceCollection extends AbstractCollection<List<String>> {
+		static class SentenceIterator implements Iterator<List<String>> {
+
+			BufferedReader reader;
+
+			public boolean hasNext() {
+				try {
+					return reader.ready();
+				} catch (IOException e) {
+					return false;
+				}
+			}
+
+			public List<String> next() {
+				try {
+					String line = reader.readLine();
+					String[] parts = line.split("\t");
+					String topic = parts[0];
+					String level = parts[1];
+					line = parts[2];
+					String[] words = line.split("\\s+");
+					List<String> sentence = new ArrayList<String>();
+					for (int i = 0; i < words.length; i++) {
+						String word = words[i];
+						sentence.add(word.toLowerCase());
+					}
+					return sentence;
+				} catch (IOException e) {
+					throw new NoSuchElementException();
+				}
+			}
+
+			public void remove() {
+				throw new UnsupportedOperationException();
+			}
+
+			public SentenceIterator(BufferedReader reader) {
+				this.reader = reader;
+			}
+		}
+
+		String fileName;
+
+		public Iterator<List<String>> iterator() {
+			try {
+				BufferedReader reader = new BufferedReader(new FileReader(
+						fileName));
+				return new SentenceIterator(reader);
+			} catch (FileNotFoundException e) {
+				throw new RuntimeException("Problem with SentenceIterator for "
+						+ fileName);
+			}
+		}
+
+		public int size() {
+			int size = 0;
+			Iterator<List<String>> i = iterator();
+			while (i.hasNext()) {
+				size++;
+				i.next();
+			}
+			return size;
+		}
+
+		public SentenceCollection(String fileName) {
+			this.fileName = fileName;
+		}
+
+		public static class Reader {
+			static Collection<List<String>> readSentenceCollection(
+					String fileName) {
+				return new SentenceCollection(fileName);
+			}
+		}
+
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Map<String, String> argMap = CommandLineUtils
 				.simpleCommandLineParser(args);
-		
+
 		String basePath = ".";
 		String model = "baseline";
 		boolean verbose = false;
-		
+
 		if (argMap.containsKey("--path")) {
 			basePath = argMap.get("-path");
 		}
-		
+
 		if (argMap.containsKey("-method")) {
 			model = argMap.get("-method");
 		}
--- src/nlp/langmodel/LanguageModel.java
+++ src/nlp/langmodel/LanguageModel.java
@@ -4,15 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 
 import nlp.util.LabelTweet;
+import nlp.util.Pair;
 
 /**
  * Language models assign probabilities to sentences and generate sentences.
  */
 public abstract class LanguageModel {
-	public void train(HashMap<String, List<LabelTweet>> traindata) {
+	public void train(HashMap<String, Pair<String, List<String>>> traindata) {
 		
 	}
-	public double predictProbability(HashMap<String, List<LabelTweet>> traindata) {
+	public double predictProbability(HashMap<String, Pair<String, List<String>>> traindata) {
 		return 0;
 	}
 	
